0857126b58db07ccf6442c49a9c0a43a61ae0b0f
hector2767 <71105207+hector2767@users.noreply.github.com>
2020-10-20 11:45:49 -0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Card {
+    private String trebol, corazones, picas, diamantes;
+    private String[] Palo = new String[]{trebol,corazones, picas, diamantes};
+
+    private String rojo, negro;
+    private String[] Color = new String[]{rojo, negro};
+
+    private int A,J,Q,K,2,3,4,5,8,6,7,9,10;
+    private String[] Valor = new String[],A,J,Q,K,2,3,4,5,8,6,7,9,10};
+
+
+}
---
+++ Deck.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Deck {
+    private int cartas = 52;
+
+
+private void pick(){
+    var i = cartas - 1;
+    }
+
+    private void shuffle(){
+        System.out.println("Se mezclo el Deck");
+    }
+}
